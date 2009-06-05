c0a0dc01e3d6a31aff9c06b07c9307097431b217
Chaz Hinkle <chazzer@webzealot.com>
2009-06-05 17:20:49 -0400

    added count update AJAX to left column

--- app/controllers/plain_controller.rb
+++ app/controllers/plain_controller.rb
@@ -10,4 +10,8 @@ class PlainController < ApplicationController
      @latest_postings = Comment.find(:all, :conditions => {:assignment_id => params[:id] }, :limit => 10, :order => "id DESC")
   end
   
+  def update_count
+    comment_count = Comment.count(:conditions => {:assignment_id => params[:id]} )
+  end
+  
 end
