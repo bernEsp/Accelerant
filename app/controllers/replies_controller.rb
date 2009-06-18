8c66c1bb42741dca696f3590f8d9065bd11090b2
Chaz Hinkle <chazzer@webzealot.com>
2009-06-18 14:16:01 -0400

    enhanced AJAX for sub comment controller

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -17,7 +17,9 @@ class RepliesController < ApplicationController
     @reply = Replies.new(params[:reply])
     @reply.save
     @assignment = Comment.find(:last, :conditions => {:id => params[:reply][:comment_id]})
-    redirect_to "/assignment/#{@assignment.assignment_id}"
+    user = User.find(:last, :conditions => {:id => self.current_user.id})
+    #redirect_to "/assignment/#{@assignment.assignment_id}"
     #render :text => @reply.content
+    render :text => "<p style='background-color:#cdd7de;margin:8px;padding:2px;' id='reply#{@reply.id}'><img src='#{user.avatar.url(:smaller)}'%>&nbsp;&nbsp;&nbsp;&nbsp;#{@reply.content}<br/></p>"
   end
 end
