abfbb3b3a0b82e7687290e83b7e260aa357cb81c
Chaz Hinkle <chazzer@webzealot.com>
2009-06-29 08:44:31 -0400

    Set WHINY to false when generating thmbnails for postings

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,10 @@ class Comment < ActiveRecord::Base
   belongs_to :project
   belongs_to :comment_assignment
   
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "50x50>", :tiny => "20x20>" }
+  has_attached_file :photo, 
+  :styles => { :medium => "300x300>", :thumb => "100x100>", :small => "50x50>", :tiny => "20x20>" }, 
+  :whiny => false,
+  :whiny_thumbnails => false
   
   comma do
 
