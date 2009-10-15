3cdb78bc490a9dd1234e3100fbe92fa8bb23481c
Chaz Hinkle <chazzer@webzealot.com>
2009-10-15 13:20:32 -0400

    Adding new login screen view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
 
+  layout 'login'
+
   if ENV['RAILS_ENV'] == 'production'
     ssl_allowed :create
   end
