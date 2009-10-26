0f7c82704a960e001b61ce16e65842eef6f93582
Chaz Hinkle <chazzer@webzealot.com>
2009-10-26 08:43:53 -0400

    added new marketing screen to front end, changed routes to default to START

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resource :session
   
-  map.root :controller => 'sessions', :action => 'new'
+  #map.root :controller => 'sessions', :action => 'new'
+  map.root :controller => 'start', :action => 'index'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
