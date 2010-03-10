59e6ceefcba13836102e7ee11b49836372019bef
Chaz Hinkle <chazzer@webzealot.com>
2010-03-10 09:04:11 -0500

    adding ActionMailer stuff

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,4 +1,28 @@
 class UserMailer < ActionMailer::Base
-  
+
+  def welcome_email(user)
+    recipients    user.email
+    from          "My Awesome Site Notifications <notifications@example.com>"
+    subject       "Welcome to My Awesome Site"
+    sent_on       Time.now
+    body          :user => user, :url => "http://example.com/login"
+  end
+
+  def receive(email)
+    page = Page.find_by_address(email.to.first)
+    page.emails.create(
+      :subject => email.subject,
+      :body => email.body
+    )
+
+    if email.has_attachments?
+      for attachment in email.attachments
+        page.attachments.create({
+          :file => attachment,
+          :description => email.subject
+        })
+      end
+    end
+  end
 
 end
