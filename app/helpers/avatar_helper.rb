bfcd3b89101f1a8d8d0de757d34c1963ff022458
Chaz Hinkle <chazzer@webzealot.com>
2010-04-01 08:50:37 -0400

    cleaning up replies avatar helper adding target = _blank

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -5,13 +5,13 @@ module AvatarHelper
       if self.current_user.participant?
         image_tag('gravatar-40.png')
       else
-        return link_to image_tag('gravatar-40.png'), :controller => 'users', :action => 'detail', :id => user.id
+        return link_to image_tag('gravatar-40.png'), {:controller => 'users', :action => 'detail', :id => user.id}, {:target => "_blank" }
       end
     else
       if self.current_user.participant?
         return image_tag(user.avatar.url(:small))
       else
-        return link_to image_tag(user.avatar.url(:small)), :controller => 'users', :action => 'detail', :id => user.id
+        return link_to image_tag(user.avatar.url(:small)), {:controller => 'users', :action => 'detail', :id => user.id}, {:target => "_blank" }
       end
     end
   end
@@ -21,13 +21,13 @@ module AvatarHelper
       if self.current_user.participant?
         image_tag('gravatar-20.png')
       else
-        return link_to image_tag('gravatar-20.png'), :controller => 'users', :action => 'detail', :id => user.id
+        return link_to image_tag('gravatar-20.png'), {:controller => 'users', :action => 'detail', :id => user.id}, {:target => "_blank" }
       end
     else
       if self.current_user.participant?
         return image_tag(user.avatar.url(:smaller))
       else
-        return link_to image_tag(user.avatar.url(:smaller)), :controller => 'users', :action => 'detail', :id => user.id
+        return link_to image_tag(user.avatar.url(:smaller)), {:controller => 'users', :action => 'detail', :id => user.id}, {:target => "_blank" }
       end
     end
   end
