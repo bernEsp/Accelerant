17733091d8e18220170cd06f4d671d64b11ca30b
Chaz Hinkle <chazzer@webzealot.com>
2010-04-07 16:22:27 -0400

    email attachments parsing and attaching with paperclip

--- app/controllers/emailer_controller.rb
+++ app/controllers/emailer_controller.rb
@@ -1,10 +1,47 @@
 class EmailerController < ApplicationController
 
   layout 'plain'
+  require 'net/imap'
+  require 'tmail'
 
   
   def index
-    
+
+
+#UserMailer.check_mail
+#UserMailer.deliver_test_email()
+imap = Net::IMAP.new('secure.emailsrvr.com')
+imap.login('study@blognogresearch.com', 'masterkey')
+imap.select('INBOX')
+imap.search(["NOT", "DELETED"]).each do |message_id|
+msg = imap.fetch(message_id,'RFC822')[0].attr['RFC822']
+mail = TMail::Mail.parse(msg)
+
+body = mail.body
+subject = mail.subject
+from = mail.from
+
+@emailing_user = User.find(:last, :conditions => {:login => subject})
+
+if @emailing_user
+
+  comment = Comment.new()
+  comment.comment = body
+  comment.user_id = @emailing_user.id
+  if ! mail.attachments.blank?
+    #File.open(mail.attachments.first.original_filename, 'rb') { |attachment| comment.photo = attachment }
+    comment.photo = mail.attachments.first
+  end
+  comment.save
+
+else
+UserMailer.deliver_not_found(from, subject)
+end
+
+
+imap.store(message_id, "+FLAGS", [:Deleted])
+end
+imap.expunge()
 
   end
 
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,3 @@
 #Paperclip.options[:command_path] = 'path\to\imagemagick\directory'
+#Paperclip.options[:command_path] = '/usr/local/bin'
 #Paperclip.options[:swallow_stderr] = false
