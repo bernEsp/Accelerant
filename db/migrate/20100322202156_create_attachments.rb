cb3e7e48cc8ac7fe40b6bdf0f6eed0abe7a40acf
Chaz Hinkle <chazzer@webzealot.com>
2010-04-09 12:15:03 -0400

    wheneverizing

--- db/migrate/20100322202156_create_attachments.rb
+++ db/migrate/20100322202156_create_attachments.rb
@@ -1,10 +1,7 @@
 class CreateAttachments < ActiveRecord::Migration
   def self.up
     drop_table :attachments
-    create_table :attachments do |t|
-
-      t.timestamps
-    end
+    
   end
 
   def self.down
