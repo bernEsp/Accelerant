87975d1e8f66aacd3f682c0a8003c3de25a92a47
Chaz Hinkle <chazzer@webzealot.com>
2010-05-12 16:24:59 -0400

    adding model and controller for THEMES and attached LOGOS

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,2 +1,27 @@
 class ThemesController < ApplicationController
+
+  if ENV['RAILS_ENV'] == 'production'
+    ssl_required :index, :new, :create, :edit, :update
+  end
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    
+  end
+
 end
