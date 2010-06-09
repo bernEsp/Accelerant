8b8511a1d12d762d980d2fe9d53fe5a73752d0a2
Chaz Hinkle <chazzer@webzealot.com>
2010-06-09 15:37:05 -0400

    changing importation script to allow for reupload of previously uploaded participants

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -27,14 +27,23 @@ class ImporterController < ApplicationController
       @tag.save
       n=1
      else
-      @user=User.new
-      @attribute=Attributes.new
-      @user.name = column[0]
-      @user.login = column[1]
-      @user.email = column[2]
-      @user.password = column[3]
-      @user.password_confirmation = column[3]
-      @user.participant = true
+      @this_user = User.find_last_by_email(column[2])
+      if @this_user.nil?
+        @user=User.new
+        @user.name = column[0]
+        @user.login = column[1]
+        @user.email = column[2]
+        @user.password = column[3]
+        @user.password_confirmation = column[3]
+        @user.participant = true
+        @attribute=Attributes.new
+        @user_assignment = UserAssignments.new
+      else
+        @user = User.find_last_by_email(column[2])
+        @attribute = Attributes.find_last_by_user_id(@user.id)
+        @user_assignment = UserAssignments.find_last_by_user_id(@user.id)
+      end
+      
       #additional fields
       @user.field_1 = column[4]
       @user.field_2 = column[5]
@@ -64,14 +73,13 @@ class ImporterController < ApplicationController
         GC.start if n%50==0
         @attribute.user_id = @user.id
         @attribute.save
-        @user_assignment = UserAssignments.new
         @user_assignment.update_attributes(:user_id => @user.id, :project_id => params[:dump][:project_id])
         @user_assignment.save
       end
      end
 
      end
-     flash.now[:notice]="CSV Import Successful,  #{n-1} new records added to data base"
+     flash.now[:notice]="CSV Import Successful,  #{n-1} new records added/updated."
   end
   
 end
