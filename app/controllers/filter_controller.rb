334aaa43ad7b4e1dec1fcd848037fd58b3889172
Chaz Hinkle <chazzer@webzealot.com>
2010-06-14 20:50:17 -0400

    filter form parsing code

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -2,5 +2,18 @@ class FilterController < ApplicationController
 
   layout 'filter'
 
+def filter_set
+  1.upto(10) { |n| 
+      params["field_#{n}"].each_key do |field_name|
+        field_name.each_with_index do |name,index|
+          if params["field_#{n}"][name] == "1"
+            puts "#{field_name} from field_#{n}"
+          end
+        end
+      end
+    }
+    render :text => "Filter set"
+  end
+
   
 end
