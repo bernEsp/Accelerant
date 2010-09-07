156042a1ba33f27a933b3ca82c9c9ab53197f806
Chaz Hinkle <chazzer@webzealot.com>
2010-09-07 15:32:59 -0400

    fixing comment posting routine

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -36,8 +36,8 @@ class RepliesController < ApplicationController
           #page << "document.getElementById('stuff').innerHTML = '';"
           #gunk = render_reply_standalone(@reply)
           #gunk = @reply.content
-          page << "document.getElementById('subCommentForm#{params[:reply][:comment_id]}').innerHTML = '#{@reply.content}';"
-          #page << "document.getElementById('stuff').innerHTML = '#{gunk}"
+          #page << "document.getElementById('subCommentForm#{params[:reply][:comment_id]}').innerHTML = '#{@reply.content}';"
+          page << "document.getElementById('subCommentForm#{params[:reply][:comment_id]}').innerHTML = 'Comment posted!';"
 
           #page << "document.getElementById('subCommentForm#{@reply.comment_id}').innerHTML = '#{gunk}"
         end
