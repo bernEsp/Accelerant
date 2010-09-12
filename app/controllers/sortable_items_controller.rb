ba3e2c41e83c7624dfe7b18323793fd6ef3e088b
Chaz Hinkle <chazzer@webzealot.com>
2010-09-12 14:54:58 -0400

    removing before_filter

--- app/controllers/sortable_items_controller.rb
+++ app/controllers/sortable_items_controller.rb
@@ -1,6 +1,6 @@
 class Sortableitems < ActiveRecord::Base
 
-  before_filter :login_required
+  #before_filter :login_required
   if ENV['RAILS_ENV'] == 'production'
     ssl_required :index, :new, :create, :edit, :update, :show, :destroy
   end
