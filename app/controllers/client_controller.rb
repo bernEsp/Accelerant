9aea0ea793d8fa6da1e4018854129574867f7671
Chaz Hinkle <chazzer@webzealot.com>
2010-09-12 15:58:17 -0400

    applying themes to moderator and client levels

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -7,5 +7,15 @@ class ClientController < ApplicationController
   def index
     client_id = self.current_user.id
     @projects = Project.find(:all, :conditions => "client_id = #{self.current_user.id}")
+
+    @last_ass = Project.find(:last, :conditions => "client_id = #{self.current_user.id}")
+    @ass = Project.find(@last_ass.id)
+    @theme = Themes.find(@ass.theme)
+
+    unless @theme.nil?
+      session[:theme] = @theme.id
+    else
+      session[:theme] = nil
+    end
   end
 end
