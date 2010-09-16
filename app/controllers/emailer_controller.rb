3393270276d88cb6cfbaec39ac283f3bd451d2be
Chaz Hinkle <chazzer@webzealot.com>
2010-09-16 16:32:43 -0400

    adding filter to comment level temporarily

--- app/controllers/emailer_controller.rb
+++ app/controllers/emailer_controller.rb
@@ -18,7 +18,12 @@ msg = imap.fetch(message_id,'RFC822')[0].attr['RFC822']
 mail = TMail::Mail.parse(msg)
 
 #body = mail.body
-body = mail.body_plain
+if mail.body_plain.nil?
+  body = mail.body_html
+else
+  body = mail.body_plain
+end
+
 subject = mail.subject
 from = mail.from
 
