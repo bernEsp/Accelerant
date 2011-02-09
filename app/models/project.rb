26c10fcec29ae07e7ee1b8e9ed41e6ec0697729d
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-02-09 14:14:00 -0600

    add storange s3 to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
   named_scope :exists, :conditions => [' 1 = 1 ']
   
   has_attached_file :photo,
- 
+  :storage => :s3,
   :styles => { :large => "300x300>", :medium => "100x100>", :small => "50x50>", :tiny => "20x20>" },
   :whiny => false,
   :whiny_thumbnails => false,
