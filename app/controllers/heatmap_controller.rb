4a424b92d0222110670a1efd2600b388c756b68e
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-02-12 10:57:28 -0600

    change heatmap create method

--- app/controllers/heatmap_controller.rb
+++ app/controllers/heatmap_controller.rb
@@ -1,26 +1,17 @@
 class HeatmapController < ApplicationController
 
   def create
-      last_comment = Comment.find(:last, :conditions => { :user_id => params[:user_id], :discussion_id => params[:discussion_id]})
-      coords = params[:encodeData].to_a
-      discussion= Discussion.find(params[:discussion_id])
-      heatmap = Heatmap.new(:image_result => params[:image_data], :user_id => params[:user_id], :comment_id => last_comment.id)
-      if heatmap.save
-        discussion.heatmaps << heatmap
-        coords.each do |coord|
-          heatmap_coords =HeatmapCoord.new(:coord_x => coord[0], :coord_y => coord[1], :coord_radio => coord[2])
-          heatmap.heatmap_coords << heatmap_coords if heatmap_coords.save
-        end
-        respond_to do |format|
-          format.html { render :nothing => true }
-          format.xml { render :xml => 'success'}
-        end
-      else
-        respond_to do |format| 
-          format.html { render :nothing => true }
-          format.xml { render :xml => 'error' }
-        end
+    if create_heatmap(params[:image_data, params[:encodeData], params[:user_id], params[:discussion_id])
+      respond_to do |format|
+        format.html { render :nothing => true }
+        format.xml { render :xml => 'success'}
       end
+    else
+      respond_to do |format| 
+        format.html { render :nothing => true }
+        format.xml { render :xml => 'error' }
+      end
+    end
   end
 
 end
--- app/models/heatmap.rb
+++ app/models/heatmap.rb
@@ -4,4 +4,25 @@ class Heatmap < ActiveRecord::Base
   belongs_to :discussion
   belongs_to :comment
   has_many :heatmap_coords
+
+  def create_heatmap(image, coords.to_a, user_id, discussion_id){
+    last_comment = Comment.find(:last, :conditions => { :user_id => user_id, \
+                                :discussion_id => discussion_id})
+    discussion= Discussion.find(discussion_id)
+    heatmap = Heatmap.new(:image_result => image, :user_id => user_id, \
+                          :comment_id => last_comment.id)
+    if heatmap.save
+       discussion.heatmaps << heatmap
+       image_coords = []
+       while coords.length >= 3
+          image_coords == coords.slice!(0..2)
+          image_coords.each do |coord|
+            heatmap_coords = HeatmapCoord.new(:coord_x => coord[0], :coord_y => coord[1], \
+                                            :coord_radio => coord[2])
+            heatmap.heatmap_coords << heatmap_coords  if heatmap_coords.save
+          end
+       end
+       return true
+    end
+  }
 end
