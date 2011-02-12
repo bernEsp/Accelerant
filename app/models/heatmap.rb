107f43d5df80b91033c01af831fdd2d4118a2ce0
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-02-12 11:12:21 -0600

    try to fix heatmap

--- app/models/heatmap.rb
+++ app/models/heatmap.rb
@@ -6,10 +6,10 @@ class Heatmap < ActiveRecord::Base
   has_many :heatmap_coords
 
   def create_heatmap(image, coords, user_id, discussion_id){
-    last_comment = Comment.find(:last, :conditions => { :user_id => user_id, \
-                                :discussion_id => discussion_id})
-    discussion= Discussion.find(discussion_id)
-    heatmap = Heatmap.new(:image_result => image, :user_id => user_id, \
+    last_comment = Comment.find(:last,
+                           :conditions {:user_id => user_id, :discussion_id => discussion_id})
+    discussion = Discussion.find(discussion_id)
+    heatmap = Heatmap.new(:image_result => image, :user_id => user_id,
                           :comment_id => last_comment.id)
     if heatmap.save
        discussion.heatmaps << heatmap
@@ -17,8 +17,8 @@ class Heatmap < ActiveRecord::Base
        while coords.to_a.length >= 3
           image_coords == coords.slice!(0..2)
           image_coords.each do |coord|
-            heatmap_coords = HeatmapCoord.new(:coord_x => coord[0], :coord_y => coord[1], \
-                                            :coord_radio => coord[2])
+            heatmap_coords = HeatmapCoord.new(:coord_x => coord[0], :coord_y => coord[1], 
+                                              :coord_radio => coord[2])
             heatmap.heatmap_coords << heatmap_coords  if heatmap_coords.save
           end
        end
