b3ce75c56b87221ce9a3bee3550d1cf0739a00ba
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-02-12 17:24:41 -0600

    add missign data to admin xml

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -24,7 +24,7 @@ class Discussion < ActiveRecord::Base
       heatmaps.each do |heatmap|
         xml_data <<  heatmap.heatmap_coords
       end
-      xml_data
+      xml_data << {:user_name => user.name, :user_id => user.id, :admin => user.admin, :image_path => discussion.media.url, :discussion_id => discussion.id }
     end
   end
 
