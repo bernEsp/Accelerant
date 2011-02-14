8fcff2c0ebaeb7e8258c196c0b7712a7be0c554c
Chaz Hinkle <chazzer@webzealot.com>
2011-02-14 10:34:14 -0500

    perception map CSV report

--- app/controllers/groupables_controller.rb
+++ app/controllers/groupables_controller.rb
@@ -1,8 +1,10 @@
 class GroupablesController < ApplicationController
 
-    before_filter :login_required
+  before_filter :login_required
+  require 'csv'
+  
   if ENV['RAILS_ENV'] == 'production'
-    ssl_required :index, :new, :create, :edit, :update, :show, :destroy
+    ssl_required :index, :new, :create, :edit, :update, :show, :destroy, :export_to_csv
   end
 
   def index
@@ -38,6 +40,31 @@ class GroupablesController < ApplicationController
     @X = Groupableaxis.find(:last, :conditions => {:orientation => "X", :groupable => params[:id]})
     @Y = Groupableaxis.find(:last, :conditions => {:orientation => "Y", :groupable => params[:id]})
     #@groupabletargets = Groupabletargets.find_all_by_groupable(params[:id], :order => "id ASC")
+  end
+
+  def export_to_csv
+     #groupable = Groupables.find(params[:id] )
+     groupableitems = Groupableitems.find_all_by_groupables(params[:id])
+     groupabletargets = Groupabletargets.find_all_by_groupable(params[:id])
+     report = StringIO.new
+        CSV::Writer.generate(report, ',') do |content|
+        content << ['',
+          groupabletargets[0].description,
+          groupabletargets[1].description,
+          groupabletargets[2].description,
+          groupabletargets[3].description]
+          groupableitems.each do |c|
+            countarray = Array.new
+            groupabletargets.each do |d|
+              usergroupable = Usergroupables.count(:conditions => {:position => d.id, :groupableitem => c.id})
+              countarray << usergroupable
+            end
+            content << [c.description,countarray[0],countarray[1],countarray[2],countarray[3]]
+          end
+        end
+       report.rewind
+       send_data(report.read,:type=>'text/csv;charset=iso-8859-1;',:filename=>'perception_map_report.csv',
+       :disposition =>'attachment', :encoding => 'utf8')
   end
 
 end
