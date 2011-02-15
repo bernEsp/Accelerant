f27f7fb301f0c9804639065a3fab362448c4ccfa
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-02-15 14:48:51 -0600

    change logic helper

--- app/helpers/assignment_helper.rb
+++ app/helpers/assignment_helper.rb
@@ -9,9 +9,9 @@ module AssignmentHelper
   end
 
   def show_heatmap(discussion)
-    if discussion.has_heatmap && self.current_user.participant
+    if discussion.has_heatmap 
       heatmap = Heatmap.find(:last, :conditions => {:discussion_id => @discussion.id , :user_id => self.current_user.id})
-      unless heatmap 
+      unless heatmap && self.current_user.participant
         true
       end
     end
