13ccafceb21f59f702a7893587b39eb5d4a5ec38
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-02-15 14:43:08 -0600

    add screen shot helper an fix show heatmap

--- app/helpers/assignment_helper.rb
+++ app/helpers/assignment_helper.rb
@@ -9,13 +9,22 @@ module AssignmentHelper
   end
 
   def show_heatmap(discussion)
-    if discussion.has_heatmap
+    if discussion.has_heatmap && user.participant
       heatmap = Heatmap.find(:last, :conditions => {:discussion_id => @discussion.id , :user_id => self.current_user.id})
       unless heatmap 
         true
       end
     end
     false
+  end
+
+  def heatmap_screenshot(discussion)
+   heatmap = discussion.heatmaps.find(:last, :conditions{:user_id => self.current_user.id})
+   if heatmap
+      heatmap.image_result
+   else
+      ""
+   end
   end
 
 end
