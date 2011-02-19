e85b649476604a42d0d973061c1cbe6bf577ea64
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-02-19 16:30:51 -0600

    delete unexpected end of sortable items

--- app/controllers/sortable_items_controller.rb
+++ app/controllers/sortable_items_controller.rb
@@ -3,6 +3,6 @@ class Sortableitems < ActiveRecord::Base
   #before_filter :login_required
 #  if ENV['RAILS_ENV'] == 'production'
     #ssl_required :index, :new, :create, :edit, :update, :show, :destroy
-  end
+  #end
 
 end
