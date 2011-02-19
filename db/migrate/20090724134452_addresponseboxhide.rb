1684299d7a127075ba6b0f6c6867dcba9b2bbd29
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-02-19 16:41:30 -0600

    change true to 1 is a integer data

--- db/migrate/20090724134452_addresponseboxhide.rb
+++ db/migrate/20090724134452_addresponseboxhide.rb
@@ -1,6 +1,6 @@
 class Addresponseboxhide < ActiveRecord::Migration
   def self.up
-    add_column :projects, :response_box_visible, :integer, :default => true
+    add_column :projects, :response_box_visible, :integer, :default => 1
   end
 
   def self.down
