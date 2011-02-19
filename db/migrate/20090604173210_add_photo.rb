c9293bb60726d89a3d1fc6c154a6405e853ae3b1
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-02-19 16:34:09 -0600

    delete limit to binary

--- db/migrate/20090604173210_add_photo.rb
+++ db/migrate/20090604173210_add_photo.rb
@@ -1,6 +1,6 @@
 class AddPhoto < ActiveRecord::Migration
   def self.up
-    add_column :comments, :data, :binary, :limit => 1.megabyte
+    add_column :comments, :data, :binary
   end
 
   def self.down
